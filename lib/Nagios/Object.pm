561784d9d7899958c69d70b2a6cf25f568e3859b
Duncan Ferguson <duncan_j_ferguson@yahoo.co.uk>
2013-02-01 16:24:37 +0000

    Update version numbers

--- lib/Nagios/Object.pm
+++ lib/Nagios/Object.pm
@@ -28,7 +28,7 @@ use Scalar::Util qw(blessed);
 
 # NOTE: due to CPAN version checks this cannot currently be changed to a
 # standard version string, i.e. '0.21'
-our $VERSION   = '46';
+our $VERSION   = '47';
 our $pre_link  = undef;
 our $fast_mode = undef;
 our %nagios_setup;
